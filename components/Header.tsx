4e35edb7a5cb3edb3164e16e54181bc8d1e4e56a
masasami <masangular0518@gmail.com>
2021-10-21 02:01:34 +0900

    レイアウト調整＆アイコン追加

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,15 @@
+import { AiOutlineMenu } from 'react-icons/ai'
+import { GiTwoCoins } from 'react-icons/gi'
+import { AiOutlineAccountBook } from 'react-icons/ai'
+import { VscAccount } from 'react-icons/vsc'
+
 const Header = () => {
   return (
-    <header className="w-full h-16 flex items-center px-2 bg-blue-500">
-      <h1 className="text-white">Header</h1>
+    <header className="w-full h-16 flex items-center px-6 bg-blue-500">
+      <AiOutlineMenu fontSize={24} className="text-white cursor-pointer mr-4" />
+      <AiOutlineAccountBook fontSize={32} className="text-white mr-2" />
+      <h1 className="text-2xl text-white">カレンダー</h1>
+      <VscAccount fontSize={32} className="text-white ml-auto" />
     </header>
   )
 }
--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,26 @@
+import { GoCalendar } from 'react-icons/go'
+import { AiOutlineTag } from 'react-icons/ai'
+import { VscGraphLine } from 'react-icons/vsc'
+
 const Nav = () => {
-  return <nav className="w-64 h-full bg-blue-200 p-2">Nav</nav>
+  return (
+    <nav className="w-64 h-full text-gray-500 border-r border-gray-300">
+      <ul>
+        <li className="flex items-center pt-6 pb-3 pl-6 pr-12 cursor-pointer">
+          <GoCalendar className="text-xl mr-4" />
+          <span className="text-sm">トップ</span>
+        </li>
+        <li className="flex items-center py-3 pl-6 pr-12 cursor-pointer">
+          <AiOutlineTag className="text-xl mr-4" />
+          <span className="text-sm">タグ</span>
+        </li>
+        <li className="flex items-center py-3 pl-6 pr-12 cursor-pointer">
+          <VscGraphLine className="text-xl mr-4" />
+          <span className="text-sm">グラフ</span>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Nav
