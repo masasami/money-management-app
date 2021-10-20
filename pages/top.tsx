2b4d9bbe989360e9b4dcd89916a4d0a37f0f06f0
masasami <masangular0518@gmail.com>
2021-10-21 00:18:10 +0900

    カレンダー表示

--- pages/top.tsx
+++ pages/top.tsx
@@ -1,10 +1,21 @@
-import Layout from 'components/Layout'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import moment from 'moment'
 import type { NextPage } from 'next'
+
+import Layout from 'components/Layout'
 
 const Top: NextPage = () => {
   return (
     <Layout>
-      <div className="w-full h-full p-2">Top</div>
+      <div className="w-full h-full p-2">
+        <Calendar
+          localizer={momentLocalizer(moment)}
+          formats={{
+            monthHeaderFormat: 'YYYY年MM月',
+          }}
+        />
+      </div>
     </Layout>
   )
 }
