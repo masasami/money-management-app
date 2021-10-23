6564574893c6cd43c77f23c4041e32df55390251
masasami <masangular0518@gmail.com>
2021-10-24 01:21:38 +0900

    tag-list 不要なスペース削除

--- pages/tag-list.tsx
+++ pages/tag-list.tsx
@@ -6,15 +6,15 @@ const TagList: NextPage = () => {
   return (
     <Layout>
       <ul>
-        <li className="flex items-center pt-6 pb-3 pl-6 pr-12 ">
+        <li className="flex items-center pt-6 pb-3 pl-6 pr-12">
           <AiOutlineTag className="text-xl mr-4" />
           <span className="text-sm">食費</span>
         </li>
-        <li className="flex items-center py-3 pl-6 pr-12 ">
+        <li className="flex items-center py-3 pl-6 pr-12">
           <AiOutlineTag className="text-xl mr-4" />
           <span className="text-sm">娯楽費</span>
         </li>
-        <li className="flex items-center py-3 pl-6 pr-12 ">
+        <li className="flex items-center py-3 pl-6 pr-12">
           <AiOutlineTag className="text-xl mr-4" />
           <span className="text-sm">水道・光熱費</span>
         </li>
