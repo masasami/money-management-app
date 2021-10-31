65e6f4d171f512cba101f4e618639679dba232bb
masasami <masangular0518@gmail.com>
2021-10-31 20:27:45 +0900

    login ログインのAPI連携＆エラー表示

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,20 +1,66 @@
 import type { NextPage } from 'next'
+import Router from 'next/router'
+import { useState } from 'react'
 import { AiOutlineAccountBook } from 'react-icons/ai'
+import { IoIosClose } from 'react-icons/io'
+
+import { User } from 'interfaces/user'
+import { apiService } from 'lib/api.service'
 
 const Login: NextPage = () => {
+  const [loginId, setLoginId] = useState('')
+  const [password, setPassword] = useState('')
+  const [isError, setIsError] = useState(false)
+
+  const login = async () => {
+    try {
+      const user = await apiService.post<User>('login', {
+        login_id: loginId,
+        password: password,
+      })
+      console.log(user)
+      Router.push('top')
+    } catch (e) {
+      console.error(e)
+      setIsError(true)
+    }
+  }
+
   return (
     <div className="w-screen h-screen flex flex-col items-center">
       <div className=" py-8">
         <AiOutlineAccountBook fontSize={48} className="text-blue-500" />
       </div>
-      <section className="w-[90vw] max-w-[308px] p-5 bg-gray-50 border border-gray-300 rounded">
+
+      {isError && (
+        <div className="w-[90vw] max-w-[320px] py-4 px-5 mb-8 text-sm bg-red-200 border border-red-500 rounded flex items-center">
+          ログインIDまたはパスワードが違います
+          <button className="cursor-pointer ml-auto" onClick={() => setIsError(false)}>
+            <IoIosClose fontSize={20} className="text-red-500" />
+          </button>
+        </div>
+      )}
+
+      <section className="w-[90vw] max-w-[320px] p-5 bg-gray-50 border border-gray-300 rounded">
         <h2>ログインID</h2>
-        <input type="text" className="w-full border border-gray-300 rounded py-1 px-4 mb-4 outline-none" />
+        <input
+          type="text"
+          className="w-full border border-gray-300 rounded py-1 px-4 mb-4 outline-none"
+          value={loginId}
+          onChange={(e) => setLoginId(e.target.value)}
+        />
 
         <h2>パスワード</h2>
-        <input type="password" className="w-full border border-gray-300 rounded py-1 px-4 mb-4 outline-none" />
+        <input
+          type="password"
+          className="w-full border border-gray-300 rounded py-1 px-4 mb-4 outline-none"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <button className="btn-main w-full justify-center">ログイン</button>
+        <button className="btn-main w-full justify-center" onClick={login}>
+          ログイン
+        </button>
       </section>
     </div>
   )
