d7742f2a06c207e41d9430854e1e1d3adf49d897
masasami <masangular0518@gmail.com>
2021-10-31 15:13:59 +0900

    api.service post, putでパラメータを渡せるように

--- lib/api.service.ts
+++ lib/api.service.ts
@@ -9,11 +9,17 @@ export const apiService = {
   get: async <T = any>(resource: string) => {
     return (await axios.get<T>(resource)).data
   },
-  post: async <T = any>(resource: string) => {
-    return (await axios.post<T>(resource)).data
+  post: async <T = any>(
+    resource: string,
+    param: { [key: string]: any } | null = null
+  ) => {
+    return (await axios.post<T>(resource, param)).data
   },
-  put: async <T = any>(resource: string) => {
-    return (await axios.put<T>(resource)).data
+  put: async <T = any>(
+    resource: string,
+    param: { [key: string]: any } | null = null
+  ) => {
+    return (await axios.put<T>(resource, param)).data
   },
   delete: async <T = any>(resource: string) => {
     return (await axios.delete<T>(resource)).data
