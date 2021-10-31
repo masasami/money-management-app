46ca67012ea38e36daeea30be4d669e87c45bdc6
masasami <masangular0518@gmail.com>
2021-10-31 23:23:11 +0900

    userの型にプロパティ追加

--- interfaces/user.ts
+++ interfaces/user.ts
@@ -1,9 +1,13 @@
 export type User = {
   id_user: number
   name: string
+  age: number
+  gender: string
+  email: string
+  icon_name: string
   login_id: string
   password: string
-  mail: string
+  dt_birth: string
   dt_create: string
   dt_update: string
 }
