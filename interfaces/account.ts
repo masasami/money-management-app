b61572b8c38ca3357ae63d0844c4ec55ae8d0277
masasami <masangular0518@gmail.com>
2021-11-03 18:43:15 +0900

    account プロパティ追加

--- interfaces/account.ts
+++ interfaces/account.ts
@@ -2,9 +2,12 @@ export type Account = {
   id_account: number
   id_user: number
   id_tag: number | null
+  content: string
   debit: number | null
   credit: number | null
   dt_account: string
   dt_create: string
   dt_update: string
+
+  title?: string
 }
