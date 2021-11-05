9fbcd8be72d9cf364f8475afdcb942376998c791
masasami <masangular0518@gmail.com>
2021-11-06 01:33:55 +0900

    atoms.ts accountをグローバルで管理できるように

--- lib/atoms.ts
+++ lib/atoms.ts
@@ -1,7 +1,13 @@
 import { atom } from 'recoil'
 import { User } from 'interfaces/user'
+import { Account } from 'interfaces/account'
 
 export const userState = atom<User | null>({
   key: 'userState',
   default: null,
+})
+
+export const accountsState = atom<Account[]>({
+  key: 'accountsState',
+  default: [],
 })
